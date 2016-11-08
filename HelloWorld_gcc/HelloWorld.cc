9482b85a37f72f524958b180f2a79609d570ae82
Pietro Chimenti <pietro.chimenti@gmail.com>
2016-11-07 23:24:19 -0200

    HelloWorld.cc doxyfied

--- HelloWorld_gcc/HelloWorld.cc
+++ HelloWorld_gcc/HelloWorld.cc
@@ -1,14 +1,18 @@
-/**
- * @file HelloWorld.cc
+/** \file HelloWorld.cc
+ *  \brief A simple Hello world in c++
+ *  \author Pietro Chimenti
+ *  \version 0.1
+ *  \date  22/08/2016
+ *
+ *  This is a simple program to illustrate a very simple c++ program.
  */
-
-
-
-// 'Hello World!' program
-// Pietro Chimenti 22/08/2016
 
 #include <iostream>
 
+/** \brief Main
+ *
+ * This main functions only print a phrase to stdout.
+ */
 int main()
 {
   std::cout << "Hello World!" << std::endl;
