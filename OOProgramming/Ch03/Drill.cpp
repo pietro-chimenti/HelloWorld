52370fd044fab64d96d1138aa95f375e9dfbe536
TestName <chimenti@localhost.localdomain>
2017-05-03 10:56:25 -0300

    A test commit

---
+++ OOProgramming/Ch03/Drill.cpp
@@ -0,0 +1,20 @@
+/** \file HelloWorld.cc
+ *  \brief A simple Hello world in c++
+ *  \author Pietro Chimenti
+ *  \version 0.1
+ *  \date  22/08/2016
+ *
+ *  This is a simple program to illustrate a very simple c++ program.
+ */
+
+#include <iostream>
+
+/** \brief Main
+ *
+ * This main functions only print a phrase to stdout.
+ */
+int main()
+{
+  std::cout << "Hello World!" << std::endl;
+  return 0;
+}
