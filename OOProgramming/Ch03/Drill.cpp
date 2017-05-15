479f4a28af8e87c5e783c312ea3f431c65a19a8c
nataliaasaa <natalia@localhost.localdomain>
2017-05-15 11:56:23 -0300

    Drill versão final

--- OOProgramming/Ch03/Drill.cpp
+++ OOProgramming/Ch03/Drill.cpp
@@ -6,24 +6,25 @@ int main()
 	cout<<"Enter the name of the person you want to write to: ";
 	string first_name;
 	cin>>first_name;
+	cout<<"Tell us the name of the friend you want to ask about: ";
+	string friend_name;
+	cin>>friend_name;
+	cout<<"Please, let me know if your friend is Female (enter f) ou Male (enter m): ";
+	char friend_sex='0';
+	cin>>friend_sex;
+	cout<<"Please enter the age of the person you are writting to: ";
+	int age;
+	cin>>age;
+	
 	cout<<"Dear "<<first_name<<",\n";
 	cout<<"How are things going in your new home? Are you enjoying it so far?\n"; 
-	string friend_name;
-	cout<<"Tell us the name of the friend you want to ask about: ";
-	cin>>friend_name;
 	cout<<"Have you seen "<<friend_name<<" lately?\n";
-	char friend_sex='0';
-	cout<<"Please, let me know if your friend is Female (enter f) ou Male (enter m): ";
-	cin>>friend_sex;
 	if (friend_sex == 'f') {
 		cout<<"If you see "<<friend_name<<" please ask her to call me.\n";
 }
 	else if (friend_sex == 'm') {
 		cout<<"If you see "<<friend_name<<" please ask him to call me.\n";
 } 
-	int age;
-	cout<<"Please enter the age of the person you are writting to: ";
-	cin>>age;
 	if (age <= 0 || age >= 110)
 		simple_error("You are kidding!");
 	else
@@ -37,5 +38,7 @@ int main()
 	else if ( age < 12 ) {
 		cout<<"Next year you will be "<<age+1<<"!\n";
 }
-	
+	cout<<"Yours sincerely,\n\n\n";
+	cout<<"Natalia.\n";
+return (0);
 }
