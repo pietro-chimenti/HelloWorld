8c8f195813ec9186a7893d238957928838b8dc80
LeoGardini <leogardini@localhost.localdomain>
2017-09-02 14:27:51 -0300

    Exercise 01: fourth 'Try This'

---
+++ OOProgramming/Ch03/ex01_d.cpp
@@ -0,0 +1,10 @@
+#include "std_lib_facilities.h"
+
+int main(){ 					// 'Main' isn't recognized by the compiler, but 'main' is.
+
+	string s = "Goodbye, cruel world!";     // 'STRING' isn't recognized by the compiler, but 'string' it is.
+	
+	cout << s << '\n'; 			// 'cOut' and 'S' isn't recognized by the compiler, but 'cout' and 's' is. 
+
+return 0;
+}
